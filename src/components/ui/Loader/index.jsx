a0663e7a8bdef14c3baa1570b7be3f8a26a82225
Andrey <andrejverkeev99@gmail.com>
2026-09-08 18:43:50 +0300

    added props

--- src/components/ui/Loader/index.jsx
+++ src/components/ui/Loader/index.jsx
@@ -1,13 +1,14 @@
 import style from './style.module.scss';
 
-const Loader = ({ size = 40 }) => {
+const Loader = ({ size = 40, className = '', ...props }) => {
   return (
     <span
-      className={style.loader}
+      className={`${style.loader} ${className}`}
       style={{
         width: size,
         height: size,
       }}
+      {...props}
     />
   );
 };
