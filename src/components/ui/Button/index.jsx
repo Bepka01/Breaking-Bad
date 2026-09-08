c7fb6c7794b2e039479d081c094e2f0ed7a8649d
Andrey <andrejverkeev99@gmail.com>
2026-09-08 17:10:38 +0300

    added button

--- src/components/ui/Button/index.jsx
+++ src/components/ui/Button/index.jsx
@@ -1,6 +1,6 @@
 import style from './style.module.scss';
 
-const Button = ({ children, variant, onClick, ...props }) => {
+const Button = ({ children, variant = 'default', onClick, ...props }) => {
   return (
     <button
       onClick={onClick}
